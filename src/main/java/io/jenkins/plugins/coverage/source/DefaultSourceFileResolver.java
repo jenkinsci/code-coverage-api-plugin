af1236aef7b590a1eec03a497c4f6aadf670c374
Scott Rogers <warren.s.rogers@nasa.gov>
2021-01-13 06:25:05 -0500

    Remove unused import

--- src/main/java/io/jenkins/plugins/coverage/source/DefaultSourceFileResolver.java
+++ src/main/java/io/jenkins/plugins/coverage/source/DefaultSourceFileResolver.java
@@ -55,7 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
