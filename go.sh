017cd8ca76b44b13e6934bd9175c45733039d060
Ulli Hafner <ullrich.hafner@gmail.com>
2021-08-12 14:57:48 +0200

    Use analysis-pom as parent and use incrementals.

---
+++ go.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+JENKINS_HOME=../docker/volumes/jenkins-home
+
+mvn clean install -Dmaven.test.skip -Denforcer.skip || { echo "Build failed"; exit 1; }
+
+echo "Installing plugin in $JENKINS_HOME"
+
+rm -rf $JENKINS_HOME/plugins/code-coverage-api*
+cp -fv target/code-coverage-api.hpi $JENKINS_HOME/plugins/code-coverage-api.jpi
+
+CURRENT_UID="$(id -u):$(id -g)"
+export CURRENT_UID
+IS_RUNNING=$(docker-compose ps -q jenkins-controller)
+if [[ "$IS_RUNNING" != "" ]]; then
+    docker-compose restart
+    echo "Restarting Jenkins (docker compose with user ID ${CURRENT_UID}) ..."
+fi
