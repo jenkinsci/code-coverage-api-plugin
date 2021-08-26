a5b3c18cff2a0b494c55fa73b05fc935b50530be
Daniel Beck <daniel-beck@users.noreply.github.com>
2021-08-26 08:43:22 +0200

    [SECURITY-2376]

--- src/main/java/io/jenkins/plugins/coverage/CompatibleObjectInputStream.java
+++ src/main/java/io/jenkins/plugins/coverage/CompatibleObjectInputStream.java
@@ -1,15 +1,19 @@
 package io.jenkins.plugins.coverage;
 
+import hudson.remoting.ClassFilter;
+import hudson.remoting.ObjectInputStreamEx;
+
 import java.io.*;
 
 /**
  * {@link io.jenkins.plugins.coverage.targets.CoverageElement} didn't have serialVersionUID. In order to keep
  * backwards compatibility, use this class to read serialized Object to avoid that serialVersionUID is different.
  */
-public class CompatibleObjectInputStream extends ObjectInputStream {
+public class CompatibleObjectInputStream extends ObjectInputStreamEx {
 
     public CompatibleObjectInputStream(InputStream in) throws IOException {
-        super(in);
+        // TODO Use a classloader in this plugin, or uberClassLoader?
+        super(in, CoverageProcessor.class.getClassLoader(), ClassFilter.DEFAULT);
     }
 
 
