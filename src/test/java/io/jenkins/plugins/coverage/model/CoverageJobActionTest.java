6fff171548bc30a08d686d9569aa71098bbf1ef1
Ulli Hafner <ullrich.hafner@gmail.com>
2021-11-03 17:17:00 +0100

    Add another test for `CoverageJobAction` that verifies the trend chart.

--- src/test/java/io/jenkins/plugins/coverage/model/CoverageJobActionTest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/CoverageJobActionTest.java
@@ -1,8 +1,14 @@
 package io.jenkins.plugins.coverage.model;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.echarts.Build;
+import edu.hm.hafner.echarts.BuildResult;
+import edu.hm.hafner.echarts.LinesChartModel;
 
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
@@ -10,6 +16,7 @@ import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 
 import static io.jenkins.plugins.coverage.model.Assertions.*;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.*;
 import static org.mockito.Mockito.*;
 
 /**
@@ -18,7 +25,6 @@ import static org.mockito.Mockito.*;
  * @author Ullrich Hafner
  */
 class CoverageJobActionTest {
-
     @Test
     void shouldIgnoreIndexIfNoActionFound() throws IOException {
         FreeStyleProject job = mock(FreeStyleProject.class);
@@ -35,23 +41,64 @@ class CoverageJobActionTest {
 
     @Test
     void shouldNavigateToLastAction() throws IOException {
-        FreeStyleProject job = mock(FreeStyleProject.class);
         FreeStyleBuild build = mock(FreeStyleBuild.class);
-        CoverageBuildAction action = mock(CoverageBuildAction.class);
-        when(action.getOwner()).thenAnswer(i -> build);
-        when(action.getUrlName()).thenReturn("coverage");
+
+        CoverageBuildAction action = creataBuildAction(build);
 
         when(build.getAction(CoverageBuildAction.class)).thenReturn(action);
         when(build.getNumber()).thenReturn(15);
+
+        FreeStyleProject job = mock(FreeStyleProject.class);
         when(job.getLastBuild()).thenReturn(build);
 
         CoverageJobAction jobAction = new CoverageJobAction(job);
-
-        assertThat(jobAction.getProject()).isSameAs(job);
 
         StaplerResponse response = mock(StaplerResponse.class);
         jobAction.doIndex(mock(StaplerRequest.class), response);
 
         verify(response).sendRedirect2("../15/coverage");
+    }
+
+    @Test
+    void shouldCreateTrendChartForLineAndBranchCoverage() throws IOException {
+        FreeStyleBuild build = mock(FreeStyleBuild.class);
+
+        CoverageBuildAction action = creataBuildAction(build);
+
+        when(build.getAction(CoverageBuildAction.class)).thenReturn(action);
+        int buildNumber = 15;
+        when(build.getNumber()).thenReturn(buildNumber);
+
+        FreeStyleProject job = mock(FreeStyleProject.class);
+        when(job.getLastBuild()).thenReturn(build);
+
+        CoverageJobAction jobAction = new CoverageJobAction(job);
+
+        List<BuildResult<CoverageBuildAction>> history = new ArrayList<>();
+        BuildResult<CoverageBuildAction> result = new BuildResult<>(new Build(buildNumber), action);
+        history.add(result);
+        LinesChartModel chart = jobAction.createChart(history, "{}");
+
+        assertThatJson(chart).node("buildNumbers").isArray().hasSize(1).containsExactly(buildNumber);
+        assertThatJson(chart).node("domainAxisLabels").isArray().hasSize(1).containsExactly("#15");
+        assertThatJson(chart).node("series").isArray().hasSize(2);
+
+        assertThatJson(chart.getSeries().get(0)).satisfies(series -> {
+            assertThatJson(series).node("name").isEqualTo("Line");
+            assertThatJson(series).node("data").isArray().containsExactly("50");
+        });
+        assertThatJson(chart.getSeries().get(1)).satisfies(series -> {
+            assertThatJson(series).node("name").isEqualTo("Branch");
+            assertThatJson(series).node("data").isArray().containsExactly("90");
+        });
+    }
+
+    private CoverageBuildAction creataBuildAction(final FreeStyleBuild build) {
+        CoverageBuildAction action = mock(CoverageBuildAction.class);
+        when(action.getOwner()).thenAnswer(i -> build);
+        when(action.getUrlName()).thenReturn("coverage");
+        when(action.getBranchCoverage()).thenReturn(new Coverage(9, 1));
+        when(action.getLineCoverage()).thenReturn(new Coverage(10, 10));
+        return action;
     }
 }
