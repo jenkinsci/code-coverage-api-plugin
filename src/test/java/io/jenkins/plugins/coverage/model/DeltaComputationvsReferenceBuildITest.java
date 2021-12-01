bbb8e5f97eba9f6254c7874eef5154c4ab211760
Bianca Böhm <bianca.boehm@yahoo.de>
2021-12-01 21:20:47 +0100

    refactor integration test for delta computation vs reference build

--- src/test/java/io/jenkins/plugins/coverage/model/DeltaComputationvsReferenceBuildITest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/DeltaComputationvsReferenceBuildITest.java
@@ -1,26 +1,17 @@
 package io.jenkins.plugins.coverage.model;
 
 import java.io.IOException;
-import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.recipes.LocalData;
 
-import edu.hm.hafner.analysis.Report;
-
-import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Result;
 import hudson.model.Run;
 
-import io.jenkins.plugins.coverage.CoverageAction;
 import io.jenkins.plugins.coverage.CoverageProcessor;
 import io.jenkins.plugins.coverage.CoveragePublisher;
 import io.jenkins.plugins.coverage.adapter.CoberturaReportAdapter;
@@ -29,28 +20,30 @@ import io.jenkins.plugins.coverage.targets.CoverageElement;
 import io.jenkins.plugins.coverage.targets.CoverageResult;
 import io.jenkins.plugins.forensics.reference.ReferenceBuild;
 import io.jenkins.plugins.util.IntegrationTestWithJenkinsPerSuite;
-import io.jenkins.plugins.util.IntegrationTestWithJenkinsPerTest;
 
 import static io.jenkins.plugins.coverage.model.Assertions.*;
 
+/**
+ * Integration test for delta computation of reference builds.
+ */
 public class DeltaComputationvsReferenceBuildITest extends IntegrationTestWithJenkinsPerSuite {
     private static final List<String> MESSAGES = Arrays.asList("Message 1", "Message 2");
 
     private static final String COBERTURA_FILE_NAME = "cobertura-higher-coverage.xml";
     private static final String COBERTURA_FILE_NAME_2 = "cobertura-lower-coverage.xml";
-@Rule
-public JenkinsRule j = new JenkinsRule();
 
-
-
+    /**
+     * Checks if delta can be computed for reference build.
+     *
+     * @throws IOException
+     *         when trying to recover coverage result
+     * @throws ClassNotFoundException
+     *         when trying to recover coverage result
+     */
     @Test
     public void checkIfReferenceBuildForDeltaComputationIsAvailable() throws IOException, ClassNotFoundException {
-        // automatisch 1. Jenkins starten
-        // automatisch 2. Plugin deployen
-        // 3a. Job erzeugen
         FreeStyleProject project = createFreeStyleProject();
         copyFilesToWorkspace(project, COBERTURA_FILE_NAME, COBERTURA_FILE_NAME_2);
-        // 3b. Job konfigurieren// 3a. Job erzeugen
 
         CoveragePublisher coveragePublisher = new CoveragePublisher();
         CoberturaReportAdapter jacocoReportAdapter = new CoberturaReportAdapter(COBERTURA_FILE_NAME);
@@ -69,9 +62,7 @@ public JenkinsRule j = new JenkinsRule();
 
         List<CoverageAdapter> coverageAdapters = new ArrayList<>();
         coveragePublisher.setAdapters(Collections.singletonList(jacocoReportAdapter2));
-        //coverageResult.getReferenceBuild();
 
-        // coveragePublisher.setFailBuildIfCoverageDecreasedInChangeRequest(true);
         project.getPublishersList().add(coveragePublisher);
 
         ReferenceBuild referenceBuild = new ReferenceBuild(firstBuild, MESSAGES);
