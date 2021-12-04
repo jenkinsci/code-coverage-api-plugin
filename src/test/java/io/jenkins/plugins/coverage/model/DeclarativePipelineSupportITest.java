63f7431a1722ca47abfe1f26e7d92e024d012ebb
Bianca Böhm <bianca.boehm@yahoo.de>
2021-12-04 16:46:52 +0100

    updating integration test for declarative pipeline support

--- src/test/java/io/jenkins/plugins/coverage/model/DeclarativePipelineSupportITest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/DeclarativePipelineSupportITest.java
@@ -4,33 +4,70 @@ import org.junit.Test;
 
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.Run;
 
 import io.jenkins.plugins.util.IntegrationTestWithJenkinsPerSuite;
+
+import static org.assertj.core.api.Assertions.*;
 
 /**
  * Integration test to check if declarative pipelines are supported.
  */
 public class DeclarativePipelineSupportITest extends IntegrationTestWithJenkinsPerSuite {
+    private static final String COBERTURA_HIGHER_COVERAGE = "cobertura-higher-coverage.xml";
+    private static final String JACOCO_FILE_NAME = "jacoco-analysis-model.xml";
 
     /**
-     * Check if declarative pipelines are supported.
+     * Check if code coverage is supported in declarative pipelines.
      */
     @Test
     public void declarativePipelineShouldRunSuccessfully() {
-        WorkflowJob job = createPipeline();
-        //TODO: ersetzen
+
+        //cobertura
+        WorkflowJob job = createPipelineWithWorkspaceFiles(COBERTURA_HIGHER_COVERAGE);
+
         job.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "\n"
                 + "    agent any\n"
+                + "\n"
                 + "    stages {\n"
-                + "        stage('Welcome Step') {\n"
-                + "            steps { \n"
-                + "                echo 'Welcome to LambdaTest'\n"
-                //command for code coverage
-                + "            }\n"
+                + "\n"
+                + "        stage('Test') {\n"
+                + "            steps {\n"
+                + "                    publishCoverage(\n"
+                + "                        adapters: [\n"
+                + "                            istanbulCoberturaAdapter('" + COBERTURA_HIGHER_COVERAGE + "')\n"
+                + "                        ],\n"
+                + "            )}\n"
                 + "        }\n"
                 + "    }\n"
                 + "}", true));
-        buildSuccessfully(job);
+        Run<?, ?> build = buildSuccessfully(job);
+        assertThat(build.getAction(CoverageBuildAction.class).getLineCoverage().getCovered()).isEqualTo(2);
+        //TODO: add assertions
+
+        //cobertura
+        WorkflowJob jobJacoco = createPipelineWithWorkspaceFiles(COBERTURA_HIGHER_COVERAGE);
+
+        jobJacoco.setDefinition(new CpsFlowDefinition("pipeline {\n"
+                + "\n"
+                + "    agent any\n"
+                + "\n"
+                + "    stages {\n"
+                + "\n"
+                + "        stage('Test') {\n"
+                + "            steps {\n"
+                + "                    publishCoverage(\n"
+                + "                        adapters: [\n"
+                + "                            jacocoAdapter('" + JACOCO_FILE_NAME + "')\n"
+                + "                        ],\n"
+                + "            )}\n"
+                + "        }\n"
+                + "    }\n"
+                + "}", true));
+
+        Run<?, ?> buildJacoco = buildSuccessfully(jobJacoco);
+        //TODO: add assertions
     }
 
 }
