406ba687c2871a4e0ad8b76c3dd45280a429cb1e
Bianca Böhm <bianca.boehm@yahoo.de>
2021-12-07 09:53:10 +0100

    refactor integration test for health reporting

--- src/test/java/io/jenkins/plugins/coverage/model/HealthReportITest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/HealthReportITest.java
@@ -11,7 +11,6 @@ import hudson.model.HealthReport;
 import hudson.model.Result;
 import hudson.model.Run;
 
-import io.jenkins.plugins.coverage.CoverageAction;
 import io.jenkins.plugins.coverage.CoveragePublisher;
 import io.jenkins.plugins.coverage.adapter.JacocoReportAdapter;
 import io.jenkins.plugins.coverage.threshold.Threshold;
@@ -25,13 +24,13 @@ import static org.assertj.core.api.Assertions.*;
 public class HealthReportITest extends IntegrationTestWithJenkinsPerSuite {
 
     private static final String JACOCO_FILE_NAME = "jacoco-analysis-model.xml";
+    private static final int UNSTABLE_LINE_THRESHOLD = 100;
 
     /**
      * Build should succeed and HealthScore is 100%.
      */
     @Test
-    public void shouldReturnSuccess() {
-
+    public void freestyleProjectShouldReturnSuccess() {
         FreeStyleProject project = createFreeStyleProject();
         copyFilesToWorkspace(project, JACOCO_FILE_NAME);
         CoveragePublisher coveragePublisher = new CoveragePublisher();
@@ -44,44 +43,14 @@ public class HealthReportITest extends IntegrationTestWithJenkinsPerSuite {
 
         Run<?, ?> build = buildWithResult(project, Result.SUCCESS);
 
-        HealthReport healthReport = build.getAction(CoverageAction.class).getHealthReport();
-        assertThat(healthReport.getScore()).isEqualTo(100);
-        assertThat(healthReport.getLocalizableDescription().toString()).isEqualTo("Coverage Healthy score is 100%");
-        assertThat(healthReport.getIconUrl()).isEqualTo("health-80plus.png");
-    }
-
-    /**
-     * Build should fail and HealthScore is 0%.
-     */
-    @Test
-    public void shouldReturnFail() {
-
-        FreeStyleProject project = createFreeStyleProject();
-        copyFilesToWorkspace(project, JACOCO_FILE_NAME);
-        CoveragePublisher coveragePublisher = new CoveragePublisher();
-        JacocoReportAdapter jacocoReportAdapter = new JacocoReportAdapter(JACOCO_FILE_NAME);
-        coveragePublisher.setAdapters(Collections.singletonList(jacocoReportAdapter));
-
-        List<Threshold> thresholds = new ArrayList<>();
-        Threshold lineThreshold = new Threshold("Line");
-        lineThreshold.setUnstableThreshold(98);
-        thresholds.add(lineThreshold);
-        coveragePublisher.setGlobalThresholds(thresholds);
-        project.getPublishersList().add(coveragePublisher);
-
-        Run<?, ?> build = buildWithResult(project, Result.UNSTABLE);
-
-        HealthReport healthReport = build.getAction(CoverageAction.class).getHealthReport();
-        assertThat(healthReport.getIconUrl()).isEqualTo("health-00to19.png");
-        assertThat(healthReport.getLocalizableDescription().toString()).isEqualTo("Coverage Healthy score is 0%");
-        assertThat(healthReport.getScore()).isEqualTo(0);
+        verifyHealthReportSuccess(build);
     }
 
     /**
      * Build should be unstable and HealthScore is 0%.
      */
     @Test
-    public void shouldReturnUnstable() {
+    public void freestyleProjectShouldReturnUnstable() {
         FreeStyleProject project = createFreeStyleProject();
         copyFilesToWorkspace(project, JACOCO_FILE_NAME);
 
@@ -91,16 +60,34 @@ public class HealthReportITest extends IntegrationTestWithJenkinsPerSuite {
 
         List<Threshold> thresholds = new ArrayList<>();
         Threshold lineThreshold = new Threshold("Line");
-        lineThreshold.setUnhealthyThreshold(95);
-        lineThreshold.setUnstableThreshold(99);
+        lineThreshold.setUnstableThreshold(UNSTABLE_LINE_THRESHOLD);
         thresholds.add(lineThreshold);
         coveragePublisher.setGlobalThresholds(thresholds);
 
         project.getPublishersList().add(coveragePublisher);
 
         Run<?, ?> build = buildWithResult(project, Result.UNSTABLE);
+        verifyHealthReportUnstable(build);
 
-        HealthReport healthReport = build.getAction(CoverageAction.class).getHealthReport();
+    }
+
+    /**
+     * Verifies details of health report of successful build.
+     * @param build which is successful
+     */
+    private void verifyHealthReportSuccess(final Run<?,?> build) {
+        HealthReport healthReport = build.getAction(CoverageBuildAction.class).getHealthReport();
+        assertThat(healthReport.getScore()).isEqualTo(100);
+        assertThat(healthReport.getLocalizableDescription().toString()).isEqualTo("Coverage Healthy score is 100%");
+        assertThat(healthReport.getIconUrl()).isEqualTo("health-80plus.png");
+    }
+
+    /**
+     * Verifies details of health report of unstable build.
+     * @param build which is unstable
+     */
+    private void verifyHealthReportUnstable(final Run<?,?> build) {
+        HealthReport healthReport = build.getAction(CoverageBuildAction.class).getHealthReport();
         assertThat(healthReport.getIconUrl()).isEqualTo("health-00to19.png");
         assertThat(healthReport.getLocalizableDescription().toString()).isEqualTo("Coverage Healthy score is 0%");
         assertThat(healthReport.getScore()).isEqualTo(0);
