da6b7c0c0289be4b0b84e4403e8e5a3ee78d249f
Bianca Böhm <bianca.boehm@yahoo.de>
2021-12-08 19:04:11 +0100

    refactor integration test for declarative pipeline support

--- src/test/java/io/jenkins/plugins/coverage/model/DeclarativePipelineSupportITest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/DeclarativePipelineSupportITest.java
@@ -1,6 +1,5 @@
 package io.jenkins.plugins.coverage.model;
 
-
 import org.junit.Test;
 
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
@@ -15,7 +14,7 @@ import static org.assertj.core.api.Assertions.*;
  * Integration test to check if declarative pipelines are supported.
  */
 public class DeclarativePipelineSupportITest extends IntegrationTestWithJenkinsPerSuite {
-    private static final String JACOCO_FILE_NAME = "jacoco-analysis-model.xml";
+    private static final String JACOCO_ANALYSIS_MODEL_XML = "jacoco-analysis-model.xml";
 
     /**
      * Check if code coverage is supported in declarative pipelines.
@@ -23,19 +22,16 @@ public class DeclarativePipelineSupportITest extends IntegrationTestWithJenkinsP
     @Test
     public void declarativePipelineSupportJacoco() {
 
-        WorkflowJob job = createPipelineWithWorkspaceFiles(JACOCO_FILE_NAME);
+        WorkflowJob job = createPipelineWithWorkspaceFiles(JACOCO_ANALYSIS_MODEL_XML);
 
         job.setDefinition(new CpsFlowDefinition("pipeline {\n"
-                + "\n"
                 + "    agent any\n"
-                + "\n"
                 + "    stages {\n"
-                + "\n"
                 + "        stage('Test') {\n"
                 + "            steps {\n"
                 + "                    publishCoverage(\n"
                 + "                        adapters: [\n"
-                + "                            jacocoAdapter('" + JACOCO_FILE_NAME + "')\n"
+                + "                            jacocoAdapter('" + JACOCO_ANALYSIS_MODEL_XML + "')\n"
                 + "                        ],\n"
                 + "            )}\n"
                 + "        }\n"
