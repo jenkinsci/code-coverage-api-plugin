5e19b8c1765962aa7d900ea98bb4970dd5562d3f
bleibdirtroy <adrian.germeck@hm.edu>
2021-12-09 14:41:53 +0100

    removed SCM from tests

--- src/test/java/io/jenkins/plugins/coverage/model/SkipPublishingOfChecksITest.java
+++ src/test/java/io/jenkins/plugins/coverage/model/SkipPublishingOfChecksITest.java
@@ -1,6 +1,5 @@
 package io.jenkins.plugins.coverage.model;
 
-import java.io.IOException;
 import java.util.Collections;
 
 import org.junit.Test;
@@ -9,7 +8,6 @@ import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import hudson.model.FreeStyleProject;
 import hudson.model.Run;
-import hudson.plugins.git.GitSCM;
 
 import io.jenkins.plugins.coverage.CoveragePublisher;
 import io.jenkins.plugins.coverage.adapter.JacocoReportAdapter;
@@ -23,17 +21,10 @@ import static org.assertj.core.api.Assertions.*;
 enum Checks { PUBLISH_CHECKS, SKIP_CHECKS}
 
 /**
- * Enum to set if SCM is used or not.
- */
-enum Sourcecode { ADD_SOURCECODE, NO_SOURCECODE}
-
-/**
  * Tests if publishing of checks can be skipped.
  */
 public class SkipPublishingOfChecksITest extends IntegrationTestWithJenkinsPerSuite {
     private static final String JACOCO_FILENAME = "jacoco-analysis-model.xml";
-    private static final String REPOSITORY_URL = "https://github.com/jenkinsci/analysis-model.git";
-    private static final String COMMIT = "6bd346bbcc9779467ce657b2618ab11e38e28c2c";
 
     /**
      * Tests publishing of checks in freestyle-project when skip publishing checks is false.
@@ -49,7 +40,7 @@ public class SkipPublishingOfChecksITest extends IntegrationTestWithJenkinsPerSu
      */
     @Test
     public void pipelinePublishingOfChecks() {
-        WorkflowJob job = getPipelineProjectWithJacoco(Checks.PUBLISH_CHECKS, Sourcecode.NO_SOURCECODE);
+        WorkflowJob job = getPipelineProjectWithJacoco(Checks.PUBLISH_CHECKS);
         checkConsoleLog(buildSuccessfully(job), Checks.PUBLISH_CHECKS);
     }
 
@@ -67,27 +58,8 @@ public class SkipPublishingOfChecksITest extends IntegrationTestWithJenkinsPerSu
      */
     @Test
     public void pipelineSkipPublishingOfChecks() {
-        WorkflowJob job = getPipelineProjectWithJacoco(Checks.SKIP_CHECKS, Sourcecode.NO_SOURCECODE);
+        WorkflowJob job = getPipelineProjectWithJacoco(Checks.SKIP_CHECKS);
         checkConsoleLog(buildSuccessfully(job), Checks.SKIP_CHECKS);
-    }
-
-    /**
-     * Tests publishing of checks with source code when skip publishing checks is false.
-     */
-    @Test
-    public void freeStylePublishingOfChecksWithRepo() throws IOException {
-        FreeStyleProject project = getFreeStyleProjectWithJacoco(Checks.PUBLISH_CHECKS);
-        project.setScm(new GitSCM(REPOSITORY_URL));
-        checkConsoleLog(buildSuccessfully(project), Checks.PUBLISH_CHECKS);
-    }
-
-    /**
-     * Tests publishing of checks with source code when skip publishing checks is false.
-     */
-    @Test
-    public void pipelinePublishingOfChecksWithRepo() {
-        WorkflowJob job = getPipelineProjectWithJacoco(Checks.PUBLISH_CHECKS, Sourcecode.ADD_SOURCECODE);
-        checkConsoleLog(buildSuccessfully(job), Checks.PUBLISH_CHECKS);
     }
 
     /**
@@ -140,13 +112,10 @@ public class SkipPublishingOfChecksITest extends IntegrationTestWithJenkinsPerSu
      *
      * @param skipPublishingChecks
      *         to set if publishing checks should be skipped
-     * @param shouldAddSCM
-     *         to set if SCM should be added
      *
      * @return build of project with scm
      */
-    private WorkflowJob getPipelineProjectWithJacoco(final Checks skipPublishingChecks, final Sourcecode shouldAddSCM) {
-        String pipelineSCMCommand = "";
+    private WorkflowJob getPipelineProjectWithJacoco(final Checks skipPublishingChecks) {
         String skipPublishingChecksValue = "";
 
         if (skipPublishingChecks == Checks.SKIP_CHECKS) {
@@ -154,19 +123,10 @@ public class SkipPublishingOfChecksITest extends IntegrationTestWithJenkinsPerSu
         }
         else if (skipPublishingChecks == Checks.PUBLISH_CHECKS) {
             skipPublishingChecksValue = "false";
-
-        }
-        if (shouldAddSCM == Sourcecode.ADD_SOURCECODE) {
-            pipelineSCMCommand = " checkout([$class: 'GitSCM', "
-                    + "branches: [[name: '" + COMMIT + "' ]],\n"
-                    + "userRemoteConfigs: [[url: '" + REPOSITORY_URL + "']],\n"
-                    + "extensions: [[$class: 'RelativeTargetDirectory', \n"
-                    + "            relativeTargetDir: 'checkout']]])\n";
         }
 
         WorkflowJob job = createPipelineWithWorkspaceFiles(JACOCO_FILENAME);
         job.setDefinition(new CpsFlowDefinition("node {"
-                + pipelineSCMCommand
                 + "    publishCoverage adapters: [jacocoAdapter('" + JACOCO_FILENAME + "')],\n"
                 + "    skipPublishingChecks: " + skipPublishingChecksValue
                 + "}", true));
