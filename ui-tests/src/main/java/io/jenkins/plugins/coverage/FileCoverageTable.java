f7b8b5583caf891bef9090561bb997791819f3a6
bncbhm <bianca.boehm@yahoo.de>
2022-01-10 10:48:16 +0100

    prepare tests of Table of file coverage in CoverageReport

---
+++ ui-tests/src/main/java/io/jenkins/plugins/coverage/FileCoverageTable.java
@@ -0,0 +1,139 @@
+package io.jenkins.plugins.coverage;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+
+/**
+ * Area that represents the issues tables in an {@link CoverageReport} page. Several issue aspects are visualized in
+ * different tables so the actual rows are composed in concrete sub-classes.
+
+ *
+ * @author Stephan Plöderl
+ */
+@SuppressFBWarnings("EI")
+public
+class FileCoverageTable  {
+    private final String ID_OF_FILE_COVERAGE_TABLE = "coverage-details";
+    private final CoverageReport coverageReport;
+    private final List<FileCoverageTableRow> tableRows = new ArrayList<>();
+    private final List<String> headers;
+    private final WebElement tableElement;
+
+
+
+        FileCoverageTable(CoverageReport coverageReport) {
+            this.coverageReport = coverageReport;
+
+
+        tableElement = coverageReport.waitFor(By.xpath("//table[@id='" + ID_OF_FILE_COVERAGE_TABLE + "' and @isLoaded='true']"));
+        headers = tableElement.findElements(By.xpath(".//thead/tr/th"))
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        updateTableRows();
+    }
+
+    /**
+     * Updates the table rows. E.g. if they are changed by toggling a details-row.
+     */
+    public final void updateTableRows() {
+        tableRows.clear();
+
+        List<WebElement> tableRowsAsWebElements = tableElement.findElements(By.xpath(".//tbody/tr"));
+        tableRowsAsWebElements.forEach(element -> tableRows.add(createRow(element)));
+    }
+
+    /**
+     * Creates the concrete table row as an object of the matching sub-class of {@link FileCoverageTableRow}. This row
+     * contains the specialized column mapping of the corresponding issues table.
+     *
+     * @param row
+     *         the WebElement representing the specific row
+     *
+     * @return the table row
+     */
+    protected FileCoverageTableRow createRow(WebElement row) {
+        return new FileCoverageTableRow(row, this);
+    }
+
+    /**
+     * Returns the table row at the given index. This row instance contains the specialized column mapping of the
+     * corresponding issues table.
+     *
+     * @param rowIndex
+     *         the number of the row to be returned
+     *
+     * @return the row
+     * @see #createRow(WebElement)
+     */
+    public FileCoverageTableRow getRow(final int rowIndex) {
+        return getTableRows().get(rowIndex);
+    }
+
+    public List<String> getColumnHeaders() {
+        return null; //getHeaders().stream().map(Header::fromTitle).collect(Collectors.toList());
+    }
+
+
+
+    /**
+     * Returns the amount of the headers for this table.
+     *
+     * @return the amount of table headers
+     */
+    public int getHeaderSize() {
+        return headers.size();
+    }
+
+    /**
+     * Returns the amount of table rows.
+     *
+     * @return the amount of table rows.
+     */
+    public int getSize() {
+        return tableRows.size();
+    }
+
+    /**
+     * Returns the table rows as List.
+     *
+     * @return the rows of the table
+     */
+    public List<FileCoverageTableRow> getTableRows() {
+        return tableRows;
+    }
+
+    /**
+     * Return the headers of the table.
+     *
+     * @return the headers
+     */
+    public List<String> getHeaders() {
+        return headers;
+    }
+
+
+    /**
+     * Performs a click on the page button to open the page of the table.
+     *
+     * @param pageNumber
+     *         the number representing the page to open
+     */
+    public void openTablePage(final int pageNumber) {
+        WebElement webElement = coverageReport.find(By.xpath("//a[@class='page-link' and @data-dt-idx='" + (pageNumber - 1) + "']"));
+        webElement.click();
+
+        coverageReport.waitFor(By.xpath("//a[@class='page-link' and @data-dt-idx='" + (pageNumber - 1) + "']/parent::li[contains(@class, 'active')]"));
+
+        updateTableRows();
+    }
+}
---
+++ ui-tests/src/main/java/io/jenkins/plugins/coverage/FileCoverageTableRow.java
@@ -0,0 +1,81 @@
+package io.jenkins.plugins.coverage;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+/**
+ * Representation of a table row displaying severity and package of an issue.
+ *
+ * @author Stephan Plöderl
+ * @author Anna-Maria Hardi
+ * @author Elvira Hauer
+ */
+class FileCoverageTableRow {
+    private final WebElement row;
+    private final FileCoverageTable table;
+
+    FileCoverageTableRow(final WebElement row, final FileCoverageTable table) {
+        this.row = row;
+        this.table = table;
+    }
+
+    final WebElement getRow() {
+        return row;
+    }
+
+    final FileCoverageTable getTable() {
+        return table;
+    }
+
+    /**
+     * Returns all possible headers representing the columns of the table.
+     *
+     * @return the headers of the table
+     */
+    List<String> getHeaders() {
+        return getTable().getHeaders();
+    }
+
+    /**
+     * Returns all table data fields in the table row.
+     *
+     * @return the table data fields
+     */
+    final List<WebElement> getCells() {
+        return getRow().findElements(By.tagName("td"));
+    }
+
+    /**
+     * Returns a specific table data field specified by the header of the column.
+     *
+     * @param header
+     *         the header text specifying the column
+     *
+     * @return the WebElement of the table data field
+     */
+    final WebElement getCell(final String header) {
+        return getCells().get(getHeaders().indexOf(header));
+    }
+
+    /**
+     * Returns the String representation of the table cell.
+     *
+     * @param header
+     *         the header specifying the column
+     *
+     * @return the String representation of the cell
+     */
+    final String getCellContent(final String header) {
+        if (!getHeaders().contains(header)) {
+            return "-";
+        }
+        return getCell(header).getText();
+    }
+
+    final boolean isDetailsRow() {
+        return getCells().size() == 1;
+    }
+}
