5e7a19348656b12db409e0e1f3bf8578bfb01e86
Adrian Germeck <adrian.germeck@stud.fh-rosenheim.de>
2022-01-16 22:00:41 +0100

    added tests

--- ui-tests/src/test/java/CoveragePublisherTest.java
+++ ui-tests/src/test/java/CoveragePublisherTest.java
@@ -4,17 +4,20 @@ import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 
 import io.jenkins.plugins.coverage.CoveragePublisher.Adapter;
 import io.jenkins.plugins.coverage.CoveragePublisher.CoveragePublisher;
+import io.jenkins.plugins.coverage.CoveragePublisher.Threshold.AdapterThreshold;
+import io.jenkins.plugins.coverage.CoveragePublisher.Threshold.AdapterThreshold.AdapterThresholdTarget;
+import io.jenkins.plugins.coverage.CoveragePublisher.Threshold.GlobalThreshold.GlobalThresholdTarget;
 
 //TODO: ueberdenken ob tests so sinn machen & ausreichen
 
 /**
- * Acceptance tests for CoveragePublisher.
- * Verifies if set options in CoveragePublisher are used and lead to excepted results.
+ * Acceptance tests for CoveragePublisher. Verifies if set options in CoveragePublisher are used and lead to excepted
+ * results.
  */
 public class CoveragePublisherTest extends UiTest {
 
     @Test
-    public void verifiesApplyThresholdRecursively() {
+    public void testApplyThresholdRecursively() {
         //TODO
     }
 
@@ -22,7 +25,7 @@ public class CoveragePublisherTest extends UiTest {
      * Verifies that job with no report fails when setFailNoReports(true).
      */
     @Test
-    public void verifiesFailOnNoReport() {
+    public void testFailOnNoReport() {
         FreeStyleJob job = jenkins.getJobs().create(FreeStyleJob.class);
         CoveragePublisher coveragePublisher = job.addPublisher(CoveragePublisher.class);
         coveragePublisher.createAdapterPageArea("Jacoco");
@@ -35,7 +38,7 @@ public class CoveragePublisherTest extends UiTest {
      * Verifies that job with decreased coverage fails when setFailBuildIfCoverageDecreasedInChangeRequest(true).
      */
     @Test
-    public void verifiesFailOnDecreasedCoverage() {
+    public void testFailOnDecreasedCoverage() {
         FreeStyleJob job = jenkins.getJobs().create(FreeStyleJob.class);
         CoveragePublisher coveragePublisher = job.addPublisher(CoveragePublisher.class);
         Adapter jacocoAdapter = coveragePublisher.createAdapterPageArea("Jacoco");
@@ -50,26 +53,59 @@ public class CoveragePublisherTest extends UiTest {
         buildWithErrors(job);
     }
 
-
+    // TODO: Auf was soll hier getestet werden. Es gibt keine Änderungen in der UI.
     @Test
-    public void verifiesSkipPublishingChecks() {
-        //TODO
+    public void testSkipPublishingChecks() {
+        FreeStyleJob job = jenkins.getJobs().create(FreeStyleJob.class);
+        CoveragePublisher coveragePublisher = job.addPublisher(CoveragePublisher.class);
+        Adapter jacocoAdapter = coveragePublisher.createAdapterPageArea("Jacoco");
+        coveragePublisher.setSkipPublishingChecks(false);
+        copyResourceFilesToWorkspace(job, RESOURCES_FOLDER);
+        jacocoAdapter.setReportFilePath(JACOCO_ANALYSIS_MODEL_XML);
+        job.save();
+        buildWithErrors(job);
+    }
+
+    /**
+     * Test if build fails if setFailUnhealthy is true and thresholds set.
+     */
+    @Test
+    public void testAdapterThresholdsAndFailOnUnhealthySetter() {
+        FreeStyleJob job = jenkins.getJobs().create(FreeStyleJob.class);
+        CoveragePublisher coveragePublisher = job.addPublisher(CoveragePublisher.class);
+        Adapter jacocoAdapter = coveragePublisher.createAdapterPageArea("Jacoco");
+        coveragePublisher.setSkipPublishingChecks(false);
+        copyResourceFilesToWorkspace(job, RESOURCES_FOLDER);
+        jacocoAdapter.setReportFilePath(JACOCO_ANALYSIS_MODEL_XML);
+        AdapterThreshold threshold = jacocoAdapter.createThresholdsPageArea(AdapterThresholdTarget.INSTRUCTION,
+                97,
+                99, false);
+        threshold.setFailUnhealthy(true);
+        job.save();
+        //TODO: Solle eigentlich Failen aber ist nur unstable
+        buildWithErrors(job);
+    }
+
+    /**
+     * Test if global thresholds are set.
+     */
+    @Test
+    public void testGlobalThresholdsAndFailSetter() {
+        FreeStyleJob job = jenkins.getJobs().create(FreeStyleJob.class);
+        CoveragePublisher coveragePublisher = job.addPublisher(CoveragePublisher.class);
+        Adapter jacocoAdapter = coveragePublisher.createAdapterPageArea("Jacoco");
+        coveragePublisher.setSkipPublishingChecks(false);
+        copyResourceFilesToWorkspace(job, RESOURCES_FOLDER);
+        jacocoAdapter.setReportFilePath(JACOCO_ANALYSIS_MODEL_XML);
+        coveragePublisher.createGlobalThresholdsPageArea(GlobalThresholdTarget.INSTRUCTION,
+                97,
+                99, false);
+        job.save();
+        buildUnstable(job);
     }
 
     @Test
-    public void verifiesAdapterThresholdsAndFailOnUnhealthySetter() {
-        //TODO
-    }
-
-
-    @Test
-    public void verifiesGlobalThresholdsAndFailSetter() {
-        //TODO
-    }
-
-
-    @Test
-    public void verifySourceFileStoringLevel(){
+    public void testSourceFileStoringLevel() {
         //TODO
     }
 
