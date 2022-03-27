0165d26f5bfc91c990c39259d244caf09e9e352c
Ulli Hafner <ullrich.hafner@gmail.com>
2022-03-27 18:00:10 +0200

    Remove test that is already part of `SourceITest`.

--- plugin/src/test/java/io/jenkins/plugins/coverage/model/DockerAndSourceCodeRenderingITest.java
+++ plugin/src/test/java/io/jenkins/plugins/coverage/model/DockerAndSourceCodeRenderingITest.java
@@ -49,11 +49,24 @@ class DockerAndSourceCodeRenderingITest extends IntegrationTestWithJenkinsPerSui
     }
 
     @Test
-    void shouldCopyAndRenderSourceCodeAndRenderingInPipelineOnAgent() {
-        WorkflowJob job = createPipelineWithGitAndJacocoAdapter("node");
-        Run<?, ?> build = verifyGitRepository(job);
+    void shouldCopyAndRenderSourceCodeAndRenderingInFreestyleJobOnAgent() throws IOException {
+        assumeThat(isWindows()).as("Running on Windows").isFalse();
 
-        verifySourceCode(build);
+        Node agent = createDockerAgent(AGENT_CONTAINER);
+        FreeStyleProject project = createFreeStyleProject();
+        project.setAssignedNode(agent);
+        copySingleFileToAgentWorkspace(agent, project, JACOCO_ANALYSIS_MODEL_FILE, JACOCO_ANALYSIS_MODEL_FILE);
+        CoveragePublisher coveragePublisher = new CoveragePublisher();
+        JacocoReportAdapter jacocoReportAdapter = new JacocoReportAdapter(JACOCO_ANALYSIS_MODEL_FILE);
+        coveragePublisher.setAdapters(Collections.singletonList(jacocoReportAdapter));
+        project.getPublishersList().add(coveragePublisher);
+        Run<?, ?> build = buildSuccessfully(project);
+
+        assertThat(build.getNumber()).isEqualTo(1);
+
+        CoverageBuildAction coverageResult = build.getAction(CoverageBuildAction.class);
+        assertThat(coverageResult.getLineCoverage())
+                .isEqualTo(new Coverage(6083, 6368 - 6083));
     }
 
     private void verifySourceCode(final Run<?, ?> build) {
@@ -91,26 +104,5 @@ class DockerAndSourceCodeRenderingITest extends IntegrationTestWithJenkinsPerSui
                 + "}", true));
 
         return job;
-    }
-
-    @Test
-    void shouldCopyAndRenderSourceCodeAndRenderingInFreestyleJobOnAgent() throws IOException {
-        assumeThat(isWindows()).as("Running on Windows").isFalse();
-
-        Node agent = createDockerAgent(AGENT_CONTAINER);
-        FreeStyleProject project = createFreeStyleProject();
-        project.setAssignedNode(agent);
-        copySingleFileToAgentWorkspace(agent, project, JACOCO_ANALYSIS_MODEL_FILE, JACOCO_ANALYSIS_MODEL_FILE);
-        CoveragePublisher coveragePublisher = new CoveragePublisher();
-        JacocoReportAdapter jacocoReportAdapter = new JacocoReportAdapter(JACOCO_ANALYSIS_MODEL_FILE);
-        coveragePublisher.setAdapters(Collections.singletonList(jacocoReportAdapter));
-        project.getPublishersList().add(coveragePublisher);
-        Run<?, ?> build = buildSuccessfully(project);
-
-        assertThat(build.getNumber()).isEqualTo(1);
-
-        CoverageBuildAction coverageResult = build.getAction(CoverageBuildAction.class);
-        assertThat(coverageResult.getLineCoverage())
-                .isEqualTo(new Coverage(6083, 6368 - 6083));
     }
 }
