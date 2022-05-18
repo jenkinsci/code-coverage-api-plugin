561b062b9d48cbd0a314fc0db62bcebd27c74a4c
Ulli Hafner <ullrich.hafner@gmail.com>
2022-05-18 09:49:20 +0200

    Add a converter for the coverage per line mappings.

--- plugin/src/test/java/io/jenkins/plugins/coverage/model/CoverageXmlStreamTest.java
+++ plugin/src/test/java/io/jenkins/plugins/coverage/model/CoverageXmlStreamTest.java
@@ -1,6 +1,7 @@
 package io.jenkins.plugins.coverage.model;
 
 import java.nio.file.Path;
+import java.util.TreeMap;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +10,9 @@ import edu.hm.hafner.util.SerializableTest;
 import io.jenkins.plugins.coverage.adapter.JacocoReportAdapter;
 import io.jenkins.plugins.coverage.adapter.JacocoReportAdapter.JacocoReportAdapterDescriptor;
 import io.jenkins.plugins.coverage.exception.CoverageException;
+import io.jenkins.plugins.coverage.model.Coverage.CoverageBuilder;
 import io.jenkins.plugins.coverage.model.CoverageBuildAction.CoverageXmlStream;
+import io.jenkins.plugins.coverage.model.CoverageBuildAction.LineMapConverter;
 import io.jenkins.plugins.coverage.targets.CoverageElementRegister;
 import io.jenkins.plugins.coverage.targets.CoverageResult;
 
@@ -21,6 +24,8 @@ import static io.jenkins.plugins.coverage.model.Assertions.*;
  * @author Ullrich Hafner
  */
 class CoverageXmlStreamTest extends SerializableTest<CoverageNode> {
+    private static final CoverageBuilder BUILDER = new CoverageBuilder();
+
     @Override
     protected CoverageNode createSerializable() {
         // TODO: replace with actual result
@@ -47,5 +52,32 @@ class CoverageXmlStreamTest extends SerializableTest<CoverageNode> {
         xmlStream.write(saved, convertedNode);
         CoverageNode restored = xmlStream.read(saved);
         assertThat(restored).isEqualTo(convertedNode);
+    }
+
+    @Test
+    void shouldConvertMap2String() {
+        TreeMap<Integer, Coverage> map = new TreeMap<>();
+
+        LineMapConverter converter = new LineMapConverter();
+
+        assertThat(converter.marshal(map)).isEmpty();
+
+        map.put(10, BUILDER.setCovered(5).setTotal(8).build());
+        assertThat(converter.marshal(map)).isEqualTo("10: 5/8");
+
+        map.put(20, BUILDER.setCovered(6).setTotal(6).build());
+        assertThat(converter.marshal(map)).isEqualTo("10: 5/8, 20: 6/6");
+    }
+
+    @Test
+    void shouldConvertString2Map() {
+        LineMapConverter converter = new LineMapConverter();
+
+        assertThat(converter.unmarshal("")).isEmpty();
+        Coverage first = BUILDER.setCovered(5).setTotal(8).build();
+        assertThat(converter.unmarshal("10: 5/8"))
+                .containsExactly(entry(10, first));
+        assertThat(converter.unmarshal("10: 5/8, 20: 6/6"))
+                .containsExactly(entry(10, first), entry(20, BUILDER.setCovered(6).setTotal(6).build()));
     }
 }
