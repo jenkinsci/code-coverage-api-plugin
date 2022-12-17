16aaca1eb19212c686569a4edf50d472322b6a56
Reshadat Ali <reshadat@avidsecure.io>
2022-12-17 20:30:14 +0530

    Fix Compilation issue

--- plugin/src/test/java/io/jenkins/plugins/coverage/CoverageProcessorTest.java
+++ plugin/src/test/java/io/jenkins/plugins/coverage/CoverageProcessorTest.java
@@ -1,22 +1,15 @@
 package io.jenkins.plugins.coverage;
 
-import io.jenkins.plugins.coverage.CoveragePublisher.CoveragePublisher;
-import io.jenkins.plugins.coverage.CoveragePublisher.CoveragePublisher.SourceFileResolver;
-import org.jenkinsci.test.acceptance.junit.WithPlugins;
-import org.jenkinsci.test.acceptance.po.Build;
-import org.jenkinsci.test.acceptance.po.FreeStyleJob;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.jvnet.hudson.test.JenkinsRule;
 
 /**
  * Acceptance tests for CoverageProcessor. Verifies if set options in CoverageProcessor are used and lead to accepted
  * results.
  */
 public class CoverageProcessorTest {
-
+    public JenkinsRule j = new JenkinsRule();
     /**
      * Test roundFloat method in CoverageProcessor
      */
@@ -26,5 +19,4 @@ public class CoverageProcessorTest {
         float scaledNumber= CoverageProcessor.roundFloat(number,2);
         Assert.assertEquals(scaledNumber,0.00);
     }
-
 }
