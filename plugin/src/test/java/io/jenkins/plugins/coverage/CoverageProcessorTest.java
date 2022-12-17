c8b60216b05f2dc6addcbf35544554feced14c43
Reshadat Ali <reshadat@avidsecure.io>
2022-12-17 20:31:27 +0530

    Remove unnecessary import

--- plugin/src/test/java/io/jenkins/plugins/coverage/CoverageProcessorTest.java
+++ plugin/src/test/java/io/jenkins/plugins/coverage/CoverageProcessorTest.java
@@ -2,14 +2,12 @@ package io.jenkins.plugins.coverage;
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
 /**
  * Acceptance tests for CoverageProcessor. Verifies if set options in CoverageProcessor are used and lead to accepted
  * results.
  */
 public class CoverageProcessorTest {
-    public JenkinsRule j = new JenkinsRule();
     /**
      * Test roundFloat method in CoverageProcessor
      */
