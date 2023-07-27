102fb8589221e0c7a385ea5359bf84243ffd4efd
aelfric-m <aelfric.mayer@gmx.at>
2023-07-27 15:24:41 +0200

    adds test for LineCoverageViewModel class

--- plugin/src/test/java/io/jenkins/plugins/coverage/metrics/steps/RenameMeTest.java
+++ plugin/src/test/java/io/jenkins/plugins/coverage/metrics/steps/RenameMeTest.java
@@ -1,8 +1,19 @@
 package io.jenkins.plugins.coverage.metrics.steps;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.core.IsCollectionContaining;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import io.jenkins.plugins.coverage.metrics.AbstractModifiedFilesCoverageTest;
+import io.jenkins.plugins.coverage.metrics.model.ChangedLinesModel;
+import io.jenkins.plugins.coverage.metrics.model.FileWithChangedLinesCoverageModel;
+import io.jenkins.plugins.coverage.metrics.model.Type;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
 
 /**
  * TODO: JavaDoc me
@@ -12,6 +23,65 @@ class RenameMeTest extends AbstractModifiedFilesCoverageTest {
     @Test
     void testSomeMethod() {
         var node = createCoverageTree();
-        // TODO: implement and rename me
+        node.filterByModifiedLines();
+        var filesWithChangedLines = LineCoverageViewModel.getFilesWithChangedLines(node);
+
+        assertThat(filesWithChangedLines.isEmpty(), is(false));
+        assertThat(filesWithChangedLines.get(0).getRelativePath(), is("test/example/Test1.java"));
+
+        for (FileWithChangedLinesCoverageModel filesWithChangedLine : filesWithChangedLines) {
+            assertThat(filesWithChangedLine.getListOfChangedLines().isEmpty(), is(false));
+        }
+
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().size(), is(4));
+
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(0).getStartLine(), is(15));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(0).getEndLine(), is(16));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(0).getType(), is(Type.COVERED));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(1).getStartLine(), is(21));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(1).getEndLine(), is(22));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(1).getType(), is(Type.COVERED));
+
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(2).getStartLine(), is(35));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(2).getEndLine(), is(36));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(2).getType(), is(Type.MISSED));
+
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(3).getStartLine(), is(20));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(3).getEndLine(), is(20));
+        assertThat(filesWithChangedLines.get(0).getListOfChangedLines().get(3).getType(), is(Type.PARTRIALLY_COVERED));
+
+        //FileWithChangedLinesCoverageModel fileOne = createTestObject("/path/Test.java", Type.COVERED, 15, 16);
+
+        var fileOneLinesList = createListOfChangedLines(Type.COVERED, 15, 16, 21, 22);
+        fileOneLinesList.addAll(createListOfChangedLines(Type.MISSED, 35, 36));
+        fileOneLinesList.addAll(createListOfChangedLines(Type.PARTRIALLY_COVERED, 20, 20));
+
+        var fileOne = new FileWithChangedLinesCoverageModel("test/example/Test1.java");
+        fileOne.setListOfChangedLines(fileOneLinesList);
+
+        assertThat(filesWithChangedLines, IsCollectionContaining.hasItem(fileOne));
+
+
+    }
+
+    private static FileWithChangedLinesCoverageModel createTestObject(String filePath, Type type, Integer... lines) {
+        FileWithChangedLinesCoverageModel fileWithChangedLines = new FileWithChangedLinesCoverageModel(filePath);
+        List<ChangedLinesModel> changedLinesModels = new ArrayList<>();
+
+        changedLinesModels.add(new ChangedLinesModel(lines[0],lines[1], type));
+        fileWithChangedLines.setListOfChangedLines(changedLinesModels);
+
+        return fileWithChangedLines;
+    }
+
+    private static List<ChangedLinesModel> createListOfChangedLines(Type type, Integer... lines) {
+        List<ChangedLinesModel> changedLinesModels = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            changedLinesModels.add(new ChangedLinesModel(lines[i], lines[i + 1], type));
+            i++;
+        }
+
+        return changedLinesModels;
     }
 }
