15a266ecf4fbcb93878f7178ffd41d89afd02521
Ulli Hafner <ullrich.hafner@gmail.com>
2023-10-23 12:02:16 +0200

    Move sources to top-level.

--- plugin/src/main/java/io/jenkins/plugins/coverage/CoverageFeatureConstants.java
+++
@@ -1,8 +0,0 @@
-package io.jenkins.plugins.coverage;
-
-
-// this class will list all extra features
-// TODO may need a better solution :(
-public class CoverageFeatureConstants {
-    public static final String FEATURE_SOURCE_FILE_PATH = "source-file-path";
-}
--- plugin/src/main/java/io/jenkins/plugins/coverage/model/visualization/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains logic and models for visualizing code coverage.
- */
-@DefaultAnnotation(NonNull.class)
-package io.jenkins.plugins.coverage.model.visualization;
-
-import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
-import edu.umd.cs.findbugs.annotations.NonNull;
